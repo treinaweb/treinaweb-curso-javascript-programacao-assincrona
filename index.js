972b7d60c6dfae435541a43d69d3d03b14f5d8ce
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-13 08:42:53 -0300

    02:07 - Exemplo prático de callback lendo arquivos

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-function getData(callback) {
-    setTimeout(() => {
-        const data = {
-            nome: "ariel",
-            idade: 29,
+const fs = require("fs");
+
+
+function lerArquivo(caminho, callback) {
+    fs.readFileSync(caminho, (erro, data) => {
+        if (erro) {
+            console.log(`Erro ao ler o arquivo: ${erro}`);
+        } else {
+            callback(data.toString());
         }
-        callback(data);
-    }, 1000)
-}
-
-
-function processData(data, callback) {
-
-
-    callback(data);
-
-}
-
-
-function displayData(data) {
-    console.log(data);
-}
-
-getData((data) => {
-    processData(data, (processesData) => {
-        displayData(processesData);
-    })
-});
-
-
-
-// CALLBACK HELL
-
-
-getUser(function (user) {
-    getProfile(user.id, function (profile) {
-        getFriends(user.id, function (friends) {
-            getMessages(user.id, function (messages) {
-                // Faz algo com user, profile, friends e messages
-            });
-        });
     });
+}
+
+lerArquivo("./arquivo.txt", (data) => {
+    console.log(data);
 });
